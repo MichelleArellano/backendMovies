73e79d6bccee0c4fcf24cc1d476a6b7f668e1d7c
Michelle Arellano <michelle.arellanofig@gmail.com>
2023-02-21 22:38:24 -0600

    feat: database connection/ create db and models and first querys and mutations

--- src/database.js
+++ src/database.js
@@ -1,14 +1,22 @@
 import mongoose from 'mongoose'
 mongoose.set('strictQuery', false)
 
-export async function connect () {
-  try {
-    await mongoose.connect('mongodb+srv://admin:1234@cluster0.jgp25ci.mongodb.net/test', {
-      useNewUrlParser: true
-    })
+// export async function connect () {
+//   try {
+//     await mongoose.connect('mongodb://localhost:27017/movies', {
+//       useNewUrlParser: true
+//     })
 
-    console.log('Database connected!!')
-  } catch (error) {
-    return console.log(error)
-  }
-}
+//     console.log('Database connected!!')
+//   } catch (error) {
+//     return console.log(error)
+//   }
+// }
+
+const uri = 'mongodb+srv://admin:1234@cluster0.jgp25ci.mongodb.net/movies'
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then((db) => console.log('DB connected!!'))
+  .catch(err => console.log(err))
